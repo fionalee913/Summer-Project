1b1d01c856bd0420baa1134c9753a3831f334899
Paulluh <paul841029@gmail.com>
2020-05-20 14:19:59 -0500

    [frontend] fixed webpack
    But dev-server broke

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Button from '@material-ui/core/Button';
+// import Checkbox from '@material-ui/core/Checkbox';
 
 function App() {
+  // const [checked, setChecked] = React.useState(true);
+  // const handleChange = (event: { target: HTMLInputElement; }) => {
+  //   setChecked(event.target.checked);
+  // };
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +26,14 @@ function App() {
         </a>
       </header>
       <p>Hello World</p>
+      <Button variant="contained" color="primary">
+        Primary
+</Button>
+      {/* <Checkbox
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      /> */}
     </div>
   );
 }
