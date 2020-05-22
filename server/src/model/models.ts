b86045ad36292d4e6dc3efcf2d598f8f31503453
Paulluh <paul841029@gmail.com>
2020-05-21 19:13:55 -0500

    [server] removed javascript artifact

--- server/src/model/models.ts
+++ server/src/model/models.ts
@@ -1,23 +1,23 @@
-export class item {
-    title: string;
-    isCompleted: boolean;
-    constructor(title: string) {
-        this.title = title;
-        this.isCompleted = false;
-    }
-}
+// export class item {
+//     title: string;
+//     isCompleted: boolean;
+//     constructor(title: string) {
+//         this.title = title;
+//         this.isCompleted = false;
+//     }
+// }
 
-export class list {
-    name: string;
-    item: item[];
-    constructor(name: string) {
-        this.name = name;
-        this.item = [];
-    }
-    addItem(i: item) {
-        this.item.push(i);
-    }
-    removeItem(i: item) {
-        
-    }
-}
+// export class list {
+//     name: string;
+//     item: item[];
+//     constructor(name: string) {
+//         this.name = name;
+//         this.item = [];
+//     }
+//     addItem(i: item) {
+//         this.item.push(i);
+//     }
+//     removeItem(i: item) {
+
+//     }
+// }
--- server/src/routes/item.ts
+++ server/src/routes/item.ts
@@ -4,17 +4,17 @@ const router = express.Router();
 router.post("/add", (req, res, next) => {
     res.send("respond with a resource");
   });
-  
-  router.post("/delete", (req, res, next) => {
-      res.send("respond with a resource");
-  });
-  
-  router.post("/update", (req, res, next) => {
-      res.send("respond with a resource");
-  });
-  
-  router.get("/", (req, res, next) => {
-      res.send("respond with a resource");
-  });
+
+router.post("/delete", (req, res, next) => {
+    res.send("respond with a resource");
+});
+
+router.post("/update", (req, res, next) => {
+    res.send("respond with a resource");
+});
+
+router.get("/", (req, res, next) => {
+    res.send("respond with a resource");
+});
 
 export default router;
