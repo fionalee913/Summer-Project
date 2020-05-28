5bbf27fbcf9bd3d59462ebba589b87a42b591ff0
Paulluh <paul841029@gmail.com>
2020-05-28 11:13:32 -0500

    [back-end] sort list by timestamp

--- server/src/models/list.ts
+++ server/src/models/list.ts
@@ -1,9 +1,10 @@
 import Item from "./item";
 
 export default class List {
-    public static parse(data: {title: string, id: string, items: Map<string, any>}): List {
+    public static parse(data: {title: string, id: string, items: Item[], timestamp: number}): List {
         const list: List = new List(data.title);
         list.id = data.id;
+        list.timestamp = data.timestamp;
         const items = Object.values(data.items).sort((a, b) => a.timestamp > b.timestamp ? 1 : -1);
         items.forEach((item) => {
             list.addItem(
@@ -15,10 +16,12 @@ export default class List {
     public title: string;
     public items: Item[];
     public id: string | null;
+    public timestamp: number;
     constructor(title: string) {
         this.title = title;
         this.items = [];
         this.id = null;
+        this.timestamp = Date.now();
     }
     private addItem(i: Item) {
         this.items.push(i);
