c4944c1e9678789599006dd7bb49d9f48893995c
Paulluh <paul841029@gmail.com>
2020-06-24 23:04:55 -0500

    [frond-end] refactored withStyle callback

--- client/src/UseStyles.tsx
+++ client/src/UseStyles.tsx
@@ -1,173 +1,69 @@
-import { fade, makeStyles, withStyles, Theme, createStyles, unstable_createMuiStrictModeTheme, ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 const drawerWidth = 240;
 
 // export const styles = (theme: any ) => ();
 
-// const style = (theme: any) => ({
-//   root: {
-//     display: 'flex',
-//   },
-//   appBar: {
-//     [theme.breakpoints.up('sm')]: {
-//       width: `calc(100% - ${drawerWidth}px)`,
-//       marginLeft: drawerWidth,
-//     },
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   title: {
-//     flexGrow: 1,
-//   },
-//   hide: {
-//     display: 'none',
-//   },
-//   toolbar: theme.mixins.toolbar,
-//   drawer: {
-//     [theme.breakpoints.up('sm')]: {
-//       width: drawerWidth,
-//       flexShrink: 0,
-//     },
-//   },
-//   drawerPaper: {
-//     width: drawerWidth,
-
-//   },
-//   content: {
-//     flexGrow: 1,
-//     padding: theme.spacing(2),
-//   },
-//   paper: {
-//     padding: theme.spacing(1),
-//     marginLeft: 280,
-//     textAlign: 'left',
-//     width: 800,
-//     color: theme.palette.text.primary,
-//   },
-//   card: {
-//     width: 800,
-//     textAlign: 'left',
-//     marginLeft: 280,
-//   },
-//   fab: {
-//     margin: theme.spacing(2),
-//   },
-//   absolute: {
-//     position: 'absolute',
-//     bottom: theme.spacing(3),
-//     right: theme.spacing(3),
-//   },
-// })
-
-export const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
+function style(theme: any): Record<any, any> {
+  return {
+    root: {
+      display: 'flex',
     },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  hide: {
-    display: 'none',
-  },
-  toolbar: theme.mixins.toolbar,
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
+    appBar: {
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    hide: {
+      display: 'none',
+    },
+    toolbar: theme.mixins.toolbar,
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    drawerPaper: {
       width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  drawerPaper: {
-    width: drawerWidth,
 
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(2),
-  },
-  paper: {
-    padding: theme.spacing(1),
-    marginLeft: 280,
-    textAlign: 'left',
-    width: 800,
-    color: theme.palette.text.primary,
-  },
-  card: {
-    width: 800,
-    textAlign: 'left',
-    marginLeft: 280,
-  },
-  fab: {
-    margin: theme.spacing(2),
-  },
-  absolute: {
-    position: 'absolute',
-    bottom: theme.spacing(3),
-    right: theme.spacing(3),
-  },
-}));
-
-export const withStyle = withStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
     },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  hide: {
-    display: 'none',
-  },
-  toolbar: theme.mixins.toolbar,
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(2),
     },
-  },
-  drawerPaper: {
-    width: drawerWidth,
+    paper: {
+      padding: theme.spacing(1),
+      marginLeft: 280,
+      textAlign: 'left',
+      width: 800,
+      color: theme.palette.text.primary,
+    },
+    card: {
+      width: 800,
+      textAlign: 'left',
+      marginLeft: 280,
+    },
+    fab: {
+      margin: theme.spacing(2),
+    },
+    absolute: {
+      position: 'absolute',
+      bottom: theme.spacing(3),
+      right: theme.spacing(3),
+    },
+  }
+}
 
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(2),
-  },
-  paper: {
-    padding: theme.spacing(1),
-    marginLeft: 280,
-    textAlign: 'left',
-    width: 800,
-    color: theme.palette.text.primary,
-  },
-  card: {
-    width: 800,
-    textAlign: 'left',
-    marginLeft: 280,
-  },
-  fab: {
-    margin: theme.spacing(2),
-  },
-  absolute: {
-    position: 'absolute',
-    bottom: theme.spacing(3),
-    right: theme.spacing(3),
-  },
-}));
+// const style = (theme: any) => ()
+
+export const useStyles = makeStyles(style);
+
+export const withStyle = withStyles(style);
